3fcfeb10d928376d1fff53d7217a2561a223a6ce
luzhiquan <13384077876@163.com>
2018-12-15 14:53:11 +0800

    代码优化

--- src/main/java/com/fotron/draw/controller/RandomController.java
+++ src/main/java/com/fotron/draw/controller/RandomController.java
@@ -1,14 +1,12 @@
 package com.fotron.draw.controller;
 
 
-import com.fotrontimes.utils.verification.annotation.SignVerification;
 import com.fotron.draw.bean.req.LuckyReq;
 import com.fotron.draw.bean.req.OpenReq;
 import com.fotron.draw.bean.resp.LuckyResp;
 import com.fotron.draw.bean.resp.OpenResp;
 import com.fotron.draw.entity.Game;
 import com.fotron.draw.entity.Goods;
-import com.fotron.draw.mapper.GoodsMapper;
 import com.fotron.draw.service.RandomService;
 import com.fotrontimes.core.web.ApiRequest;
 import com.fotrontimes.core.web.ApiResponse;
@@ -18,14 +16,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import sun.net.www.protocol.http.HttpURLConnection;
 
 import javax.annotation.Resource;
-import java.util.Date;
 
 /**
  * @author luzhiquan
